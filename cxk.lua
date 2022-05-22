b32ea7557c5198d9649fa385f117eac38359e936
bubatz <71530056+cynhook@users.noreply.github.com>
2022-05-21 20:39:29 -0400

    Create cxk.lua

---
+++ cxk.lua
@@ -0,0 +1,9 @@
+game.StarterGui:SetCore(
+    "SendNotification",
+    {
+	Title = "Neverlockin BETA 0.1v",
+	Text = "Anti Lock enabled",
+}
+)
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/neverlackinga/antilock/main/wasd.lua")()
